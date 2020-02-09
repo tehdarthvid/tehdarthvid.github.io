893e8521243a4fc9e69ec2454963da73871b9586
tehdarthvid <darthvid@hotmail.com>
2020-02-10 00:40:00 +0800

    初め!

---
+++ src/App.svelte
@@ -0,0 +1,57 @@
+<script>
+
+</script>
+
+<style>
+  @import url("https://fonts.googleapis.com/css?family=Lexend+Deca|Noto+Sans+JP|Source+Code+Pro|Ubuntu&display=swap");
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+  main {
+    text-align: center;
+  }
+  #build {
+    font-size: 0.38195em;
+  }
+</style>
+
+<main>
+  <h1>
+    <a href="/">darthvid</a>
+  </h1>
+  <p>サイトへようこそ!</p>
+  <h2>social</h2>
+  <div>
+    【
+    <a href="https://twitter.com/darthvid">Twitter</a>
+    】
+  </div>
+  <div>
+    【
+    <a href="https://github.com/tehdarthvid">GitHub</a>
+    】
+  </div>
+  <div>
+    【
+    <a href="https://www.linkedin.com/in/darthvid/">LinkedIn</a>
+    】
+  </div>
+  <h2>projects</h2>
+  <div>
+    【
+    <a href="https://dna.darthvid.com">influences</a>
+    】
+  </div>
+
+  <div id="build">
+    &nbsp;
+    <p>
+
+      <a href="https://github.com/tehdarthvid/darthvid.com">try</a>
+
+    </p>
+  </div>
+
+</main>
