8b1c6a14995b8cf161c2537ef0ccd54709e1ab1b
tehdarthvid <darthvid@hotmail.com>
2020-02-10 12:43:01 +0800

    remnants!

--- bin/update-gh-pages.sh
+++ bin/update-gh-pages.sh
@@ -10,7 +10,7 @@ set -eu
 repo_uri="https://x-access-token:${DEPLOY_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 remote_name="origin"
 main_branch="master"
-target_branch="gh-pages"
+target_branch="master"
 build_dir="public"
 
 cd "$GITHUB_WORKSPACE"
