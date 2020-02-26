17a722cbc4ccf608ee20aeaa2483106654ec8903
tehdarthvid <darthvid@hotmail.com>
2020-02-26 15:48:15 +0800

    so that there are some images on the bare site :P

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Icon from "./components/icon.svelte";
   import { googleAnalytics } from "./utils/google-analytics.js";
   if (process.env.gaID) googleAnalytics(process.env.gaID);
 
@@ -20,6 +21,12 @@
   #build {
     font-size: 0.38195em;
   }
+
+  .wrapus {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
 </style>
 
 <main>
@@ -28,26 +35,95 @@
   </h1>
   <p>サイトへようこそ!</p>
   <h2>social</h2>
-  <div>
-    【
-    <a href="https://twitter.com/darthvid">Twitter</a>
-    】
+  <div class="wrapus">
+    <div>
+      【
+      <a href="https://twitter.com/darthvid">Twitter</a>
+      】
+    </div>
+    <div>
+      【
+      <a href="https://github.com/tehdarthvid">GitHub</a>
+      】
+    </div>
+    <div>
+      【
+      <a href="https://www.linkedin.com/in/darthvid/">LinkedIn</a>
+      】
+    </div>
   </div>
-  <div>
-    【
-    <a href="https://github.com/tehdarthvid">GitHub</a>
-    】
-  </div>
-  <div>
-    【
-    <a href="https://www.linkedin.com/in/darthvid/">LinkedIn</a>
-    】
-  </div>
+
   <h2>projects</h2>
-  <div>
-    【
-    <a href="https://dna.darthvid.com">influences</a>
-    】
+  <div class="wrapus">
+    <div>
+      【
+      <a href="https://dna.darthvid.com">influences</a>
+      】
+    </div>
+  </div>
+
+  <h2>dabbles-in</h2>
+  <div class="wrapus">
+    <Icon
+      title="Node.js"
+      itemURL="https://nodejs.org/"
+      imgURL="/assets/images/nodejs.svg" />
+    <Icon
+      title="Amazon Web Services"
+      itemURL="https://aws.amazon.com/"
+      imgURL="/assets/images/aws.png" />
+    <Icon
+      title="C"
+      itemURL="http://csapp.cs.cmu.edu/3e/docs/chistory.html"
+      imgURL="/assets/images/clang.jpg" />
+  </div>
+  <div class="wrapus">
+    <Icon
+      title="Google Cloud Platform"
+      itemURL="https://cloud.google.com/"
+      imgURL="/assets/images/gcp.png" />
+    <Icon
+      title="Svelte"
+      itemURL="https://svelte.dev/"
+      imgURL="/assets/images/svelte.png" />
+    <Icon
+      title="Vue.js"
+      itemURL="https://vuejs.org/"
+      imgURL="/assets/images/vue.png" />
+  </div>
+  <div class="wrapus">
+    <Icon
+      title="fish"
+      itemURL="https://fishshell.com/"
+      imgURL="/assets/images/fish.png" />
+    <Icon
+      title="kitty"
+      itemURL="https://sw.kovidgoyal.net/kitty/"
+      imgURL="/assets/images/kitty.png" />
+    <Icon
+      title="Visual Studio Code"
+      itemURL="https://code.visualstudio.com/"
+      imgURL="/assets/images/vscode.png" />
+  </div>
+  <div class="wrapus">
+    <Icon
+      title="GitHub Actions"
+      itemURL="https://github.com/features/actions"
+      imgURL="/assets/images/gh-actions.svg" />
+    <Icon
+      title="GitLab CI/CD"
+      itemURL="https://docs.gitlab.com/ee/ci/introduction/"
+      imgURL="/assets/images/gl-cicd.png" />
+  </div>
+  <div class="wrapus">
+    <Icon
+      title="Go"
+      itemURL="https://golang.org/"
+      imgURL="/assets/images/go-gopher.png" />
+    <Icon
+      title="Rust"
+      itemURL="https://www.rust-lang.org/"
+      imgURL="/assets/images/rust-ferris.svg" />
   </div>
 
   <div id="build">
