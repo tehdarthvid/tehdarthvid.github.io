0bd5b53e6fbd464c3a219a250a8c816a525b90a1
tehdarthvid <darthvid@hotmail.com>
2020-02-26 15:47:40 +0800

    encapsulate icons for a bunch of stuff

---
+++ src/components/icon.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let title;
+  export let itemURL;
+  export let imgURL;
+</script>
+
+<style scoped>
+  .icon {
+    height: 8vh;
+  }
+</style>
+
+<div class="icon">
+  <a href={itemURL} {title} target="_blank">
+    <img class="icon" alt={title} src={imgURL} />
+  </a>
+</div>
