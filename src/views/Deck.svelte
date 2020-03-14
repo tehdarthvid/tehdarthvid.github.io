b834d10ee03c51d1414d41830dc8d19fa1383cce
tehdarthvid <darthvid@hotmail.com>
2020-03-14 22:15:12 +0800

    Deck n Card cmpnts for new "recent finds" section

---
+++ src/views/Deck.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Card from "../components/Card.svelte";
+  import { deck } from "../stores/deckStore.js";
+</script>
+
+<style scoped>
+  .container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+</style>
+
+<div>
+  {#if null != $deck}
+    <h2>recent finds</h2>
+    <div class="container">
+      {#each $deck as card}
+        <Card {...card} />
+      {/each}
+    </div>
+  {/if}
+</div>
