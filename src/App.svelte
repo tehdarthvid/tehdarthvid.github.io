388f942a6055463c4b83bdb82f54172ed33cd2c4
tehdarthvid <darthvid@hotmail.com>
2020-03-29 22:52:12 +0800

    `header/main/footer` elemsnets for accessibility.

--- src/App.svelte
+++ src/App.svelte
@@ -26,12 +26,9 @@
 
 <style>
   @import url("https://fonts.googleapis.com/css?family=Lexend+Deca|Noto+Sans+JP|Source+Code+Pro|Ubuntu&display=swap");
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-  main {
+  header,
+  main,
+  footer {
     text-align: center;
   }
   #build {
@@ -45,64 +42,62 @@
   }
 </style>
 
+<header>
+  {#if null != site_data.name}
+    <h1>
+      <a href="/">{site_data.name}</a>
+    </h1>
+  {/if}
+  {#if null != site_data.welcome_msg}
+    <p>{site_data.welcome_msg}</p>
+  {/if}
+</header>
+
 <main>
-  {#if null != site_data}
-    {#if null != site_data.name}
-      <h1>
-        <a href="/">{site_data.name}</a>
-      </h1>
-    {/if}
-    {#if null != site_data.welcome_msg}
-      <p>{site_data.welcome_msg}</p>
-    {/if}
+  {#if null != site_data.links}
+    <h2>is at</h2>
+    <div class="wrapus">
+      {#each site_data.links as linkData}
+        <LinkTo {...linkData} />
+      {/each}
+    </div>
+  {/if}
 
-    {#if null != site_data.links}
-      <h2>is at</h2>
-      <div class="wrapus">
-        {#each site_data.links as linkData}
-          <LinkTo {...linkData} />
-        {/each}
-      </div>
-    {/if}
-
-    <Deck />
-    <!--
+  <Deck />
+  <!--
     <Deck />
   -->
 
-    {#if null != site_data.projects}
-      <h2>pet projects</h2>
-      <div class="wrapus">
-        {#each site_data.projects as linkData}
-          <LinkTo {...linkData} />
-        {/each}
-      </div>
-    {/if}
-
-    {#if null != site_data.dabbles}
-      <h2>dabbles in</h2>
-      <div class="wrapus">
-        {#each site_data.dabbles as linkData}
-          <Icon {...linkData} />
-        {/each}
-      </div>
-    {/if}
+  {#if null != site_data.projects}
+    <h2>pet projects</h2>
+    <div class="wrapus">
+      {#each site_data.projects as linkData}
+        <LinkTo {...linkData} />
+      {/each}
+    </div>
   {/if}
 
+  {#if null != site_data.dabbles}
+    <h2>dabbles in</h2>
+    <div class="wrapus">
+      {#each site_data.dabbles as linkData}
+        <Icon {...linkData} />
+      {/each}
+    </div>
+  {/if}
+</main>
+
+<footer>
   <div id="build">
     &nbsp;
     <p>
-
       <a href="https://github.com/tehdarthvid/tehdarthvid.github.io/tree/dev">
         source
       </a>
-
     </p>
     <a
       href="https://github.com/tehdarthvid/tehdarthvid.github.io/commit/{git_hash}">
       build {strGitHash}
     </a>
-
   </div>
-
-</main>
+</footer>
