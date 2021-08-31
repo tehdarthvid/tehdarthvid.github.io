c564750ab01985006d459a9af2e9b3463baba8ad
tehdarthvid <darthvid@hotmail.com>
2021-08-31 15:40:18 +0800

    fix: Added env passing error handling.

--- src/main.js
+++ src/main.js
@@ -1,18 +1,24 @@
 import App from "./App.svelte";
 
+let envGAID = "err1";
 if ("undefined" == typeof ENV_GAID) {
   console.log("Error Code 1.");
+} else {
+  envGAID = ENV_GAID;
 }
 
+let envGITHASH = "err2";
 if ("undefined" == typeof ENV_GITHASH) {
-  console.log("Error Code 1.");
+  console.log("Error Code 2.");
+} else {
+  envGITHASH = ENV_GITHASH;
 }
 
 const app = new App({
   target: document.body,
   props: {
-    gaID: ENV_GAID,
-    git_hash: ENV_GITHASH,
+    gaID: envGAID,
+    git_hash: envGITHASH,
   },
 });
 
