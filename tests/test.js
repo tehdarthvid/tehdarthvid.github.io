ef6343a9b4913d89c2c7fc30e3bb7ed630457d4f
tehdarthvid <darthvid@hotmail.com>
2022-12-16 14:45:31 +0800

    test: Playing with the tests.

--- tests/test.js
+++ tests/test.js
@@ -7,19 +7,36 @@ test('index page has expected h1', async ({ page }) => {
 });
 */
 
-test('index page has minimum components', async ({ page }) => {
-	await page.goto('/');
+/* 
+	Parallel tests on this file.
+	By default, test in a file are run serial, but are parallel between files.
+*/
+test.describe.configure({ mode: 'parallel' });
 
-	// header
-	expect(await page.textContent('h1')).toBe('darthvid');
+test('has title and header', async ({ page }) => {
+	await page.goto('/');
 
 	// Expect a title "to contain" a substring.
 	await expect(page).toHaveTitle(/darthvid/);
 
+	// header
+	expect(await page.textContent('h1')).toBe('darthvid');
+
 	// welcome statement
 	expect(await page.textContent('p')).toContain('サイトへようこそ!');
+});
+
+test('has has footer', async ({ page }) => {
+	await page.goto('/');
 
 	// footer
 	await expect(page.locator('text=source')).toContainText('source');
 	await expect(page.locator('text=build')).toContainText('build');
+});
+
+test('has has all content', async ({ page }) => {
+	await page.goto('/');
+
+	await expect(page.locator('text=LinkedIn')).toContainText('LinkedIn');
+	await expect(page.locator('text=this site')).toContainText('this site');
 });
