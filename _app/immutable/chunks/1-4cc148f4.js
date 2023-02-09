a2a7b760058876f94178cabb981f8ce469f2d4a6
tehdarthvid <tehdarthvid@bots.github.com>
2023-02-09 13:37:58 +0000

    updating from 9975d8bdecbb0b23448e135a06eab3024385a1da

---
+++ _app/immutable/chunks/1-4cc148f4.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-be4dd38c.js";export{t as component};
--- _app/immutable/chunks/2-679fc60d.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-cbaae079.js";export{t as component};
