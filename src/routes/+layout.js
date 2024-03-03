5f4dd94884cf966d7663692a2fbd9b1d44286abe
tehdarthvid <darthvid@hotmail.com>
2024-03-03 20:35:19 +0800

    chore: update major versions.

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,3 +1,4 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
+// This can be false if you're using a fallback (i.e. SPA mode)
 export const prerender = true;
